2c20d99052e89ee9d6ff6912428e43ad08d84195
Levi Barreto Araújo <114665569+LeviGitH@users.noreply.github.com>
2023-02-17 00:50:13 -0300

    add: finishing

---
+++ src/components/ModalCreateTech/style.js
@@ -0,0 +1,108 @@
+import styled from "styled-components";
+
+export const StyleModalCreateTech = styled.dialog`
+  width: 100%;
+  height: 100%;
+  border: 0;
+  padding: 0;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  background: rgba(18, 18, 20, 0.5);
+  .modal__container {
+    width: 100%;
+    height: 100%;
+    max-width: 369px;
+    max-height: 342px;
+    padding: 5px;
+
+    box-shadow: 0px 4px 40px -10px rgba(0, 0, 0, 0.25);
+    .modal__header {
+      padding: 10px;
+
+      background-color: var(--grey-2);
+      border-radius: 4px 4px 0 0;
+
+      display: flex;
+      justify-content: space-between;
+      h1 {
+        font-family: "Inter";
+        font-weight: 700;
+        font-size: 0.875rem;
+        color: var(--grey-0);
+      }
+      button {
+        background-color: transparent;
+        border: none;
+
+        cursor: pointer;
+      }
+    }
+    .modal__main {
+      padding: 10px;
+
+      background-color: var(--grey-3);
+      border-radius: 0 0 4px 4px;
+      div {
+        margin-bottom: 15px;
+
+        display: flex;
+        flex-direction: column;
+        gap: 12px;
+        label {
+          font-family: "Inter";
+          font-weight: 400;
+          font-size: 0.7rem;
+          color: var(--grey-0);
+        }
+        input,
+        select {
+          height: 38.5px;
+          padding-left: 13.03px;
+
+          outline: none;
+          background-color: var(--grey-2);
+          border: 1px solid var(--grey-2);
+          border-radius: 4px;
+
+          font-family: "Inter";
+          font-weight: 400;
+          font-size: 0.814rem;
+          color: var(--grey-0);
+        }
+        input:focus {
+          border: 1px solid var(--grey-0);
+        }
+        select:focus {
+          border: 1px solid var(--grey-0);
+        }
+        p {
+          font-family: "Inter";
+          font-weight: 400;
+          font-size: 0.7rem;
+          color: var(--grey-1);
+        }
+      }
+      button {
+        width: 100%;
+        height: 38.5px;
+
+        background-color: var(--color-primary);
+        border: none;
+        border-radius: 4px;
+
+        font-family: "Inter";
+        font-weight: 500;
+        font-size: 12.8347px;
+        color: var(--white-neutral);
+
+        cursor: pointer;
+      }
+      button:hover {
+        background-color: var(--color-primary-focus);
+      }
+    }
+  }
+`;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -6,73 +6,99 @@ import { useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 
 import { Header } from "../../components/Header";
+import { ModalCreateTech } from "../../components/ModalCreateTech";
 
 export const Home = () => {
   const [user, setUser] = useState({ name: "", module: "", techs: [] });
+  const [modalOn, setModalOn] = useState({ add: "", update: "" });
   const navigate = useNavigate();
+  const token = localStorage.getItem("@TOKEN");
+
+  async function loadUser() {
+    try {
+      const headers = {
+        Authorization: `Bearer ${token}`,
+      };
+      const response = await api.get("/profile", { headers });
+      setUser({
+        name: response.data.name,
+        module: response.data.course_module,
+        techs: response.data.techs,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
   useEffect(() => {
-    const token = localStorage.getItem("@TOKEN");
     if (token === null) {
       navigate("/login");
     } else {
-      async function loadUser() {
-        try {
-          const headers = {
-            Authorization: `Bearer ${token}`,
-          };
-          const response = await api.get("/profile", { headers });
-          setUser({
-            name: response.data.name,
-            module: response.data.course_module,
-            techs: response.data.techs,
-          });
-        } catch (error) {
-          console.log(error);
-        }
-      }
       loadUser();
     }
   }, []);
+
   return (
-    <StyleHome>
-      <nav>
-        <img src={Logo} alt="Kenzie Hub Logo" />
-        <button
-          onClick={() => {
-            localStorage.clear();
-            document.location.reload(true);
-          }}
-        >
-          Sair
-        </button>
-      </nav>
-      <Header name={user.name} module={user.module} />
-      {user.techs.length > 0 ? (
-        <div className="content__container">
-          <div className="content__header">
-            <h1>Tecnologias</h1>
-            <button>
-              <img src={ButtonPlus} />
-            </button>
+    <div>
+      {modalOn.add === "working" ? (
+        <ModalCreateTech
+          setModalOn={setModalOn}
+          modalOn={modalOn}
+          token={token}
+          loadUser={loadUser}
+        />
+      ) : null}
+      <StyleHome>
+        <nav>
+          <img src={Logo} alt="Kenzie Hub Logo" />
+          <button
+            onClick={() => {
+              localStorage.clear();
+              document.location.reload(true);
+            }}
+          >
+            Sair
+          </button>
+        </nav>
+        <Header name={user.name} module={user.module} />
+        {user.techs.length > 0 ? (
+          <div className="content__container">
+            <div className="content__header">
+              <h1>Tecnologias</h1>
+              <button
+                onClick={() => {
+                  setModalOn({ ...modalOn, add: "working" });
+                }}
+              >
+                <img src={ButtonPlus} />
+              </button>
+            </div>
+            <div className="content__main">
+              {user.techs.map((element, index) => {
+                return (
+                  <button key={index}>
+                    <h1>{element.title}</h1>
+                    <h2>{element.status}</h2>
+                  </button>
+                );
+              })}
+            </div>
           </div>
-          <div className="content__main">
-            <h1>Tem tec</h1>
+        ) : (
+          <div className="content__container">
+            <div className="content__header">
+              <h1>Tecnologias</h1>
+              <button
+                onClick={() => {
+                  setModalOn({ ...modalOn, add: "working" });
+                }}
+              >
+                <img src={ButtonPlus} />
+              </button>
+            </div>
           </div>
-        </div>
-      ) : (
-        <div className="content__container">
-          <div className="content__header">
-            <h1>Tecnologias</h1>
-            <button>
-              <img src={ButtonPlus} />
-            </button>
-          </div>
-          <div className="content__main">
-            <h1>Não tem tec</h1>
-          </div>
-        </div>
-      )}
-    </StyleHome>
+        )}
+      </StyleHome>
+    </div>
   );
 };
